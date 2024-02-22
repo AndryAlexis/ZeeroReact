8e43b3bfd19319b1ea4f9cbf303642149d37d4da
AndryAlexis <sr.andryalexisreyescruz@gmail.com>
2024-02-22 17:36:35 +0100

    github pages
    lo he vuelto a dejar para que funcione ahí

--- zeero/src/App.jsx
+++ zeero/src/App.jsx
@@ -1,60 +1,60 @@
-// import {
-//   createHashRouter,
-//   RouterProvider
-// } from 'react-router-dom'
+import {
+  createHashRouter,
+  RouterProvider
+} from 'react-router-dom'
 
-// import Home from './routes/home'
-// import Projects from './routes/projects'
-// import AboutUs from './routes/aboutUs'
-// import Contact from './routes/contact'
+import Home from './routes/home'
+import Projects from './routes/projects'
+import AboutUs from './routes/aboutUs'
+import Contact from './routes/contact'
 
-// import CookieConsent from './components/cookieConsent'
+import CookieConsent from './components/cookieConsent'
 
-// const App = () => { 
-//   const router = createHashRouter(
-//     [
-//       {
-//         path: '/',
-//         element: <Home/>
-//       },
-//       {
-//         path: '/projects',
-//         element: <Projects/>
-//       },
-//       {
-//         path: '/about-us',
-//         element: <AboutUs/>
-//       },
-//       {
-//         path: '/contact',
-//         element: <Contact/>
-//       }
-//     ]
-//   )
+const App = () => { 
+  const router = createHashRouter(
+    [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/projects',
+        element: <Projects/>
+      },
+      {
+        path: '/about-us',
+        element: <AboutUs/>
+      },
+      {
+        path: '/contact',
+        element: <Contact/>
+      }
+    ]
+  )
 
-//   return <>
-//     <RouterProvider router={router}/>
-//     <CookieConsent/>
-//   </>
-// }
-
-// export default App
-//Descomenta "base" de vite.config.js
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/home';
-import CookieConsent from './components/cookieConsent';
-
-const App = () => {
   return <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          {/* <Route path="*" element={<div>Error 404 - Page not found</div>}/> */}
-        </Routes>
-      </Router>
-      <CookieConsent />
+    <RouterProvider router={router}/>
+    <CookieConsent/>
   </>
 }
 
-export default App;
+export default App
+//Descomenta "base" de vite.config.js
+
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Home from './routes/home';
+// import CookieConsent from './components/cookieConsent';
+
+// const App = () => {
+//   return <>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Home/>} />
+//           {/* <Route path="*" element={<div>Error 404 - Page not found</div>}/> */}
+//         </Routes>
+//       </Router>
+//       <CookieConsent />
+//   </>
+// }
+
+// export default App;
--- zeero/vite.config.js
+++ zeero/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: 'https://Ssadry.github.io/ZeeroReact' // Esto me sirve subirlo en github pages
+  base: 'https://Ssadry.github.io/ZeeroReact' // Esto me sirve subirlo en github pages
 })
