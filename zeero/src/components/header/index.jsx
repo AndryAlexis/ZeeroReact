4df1f84a50af20278d4f57b1a5ff4bd72cc8aeef
AndryAlexis <sr.andryalexisreyescruz@gmail.com>
2024-02-26 15:09:32 +0100

    Mejorando el código 1
    De momento solo he mejorado los componentes "home" y "header". Ahora son más legibles

--- zeero/src/components/header/index.jsx
+++ zeero/src/components/header/index.jsx
@@ -1,51 +1,71 @@
 import { Link } from "react-router-dom"
 import { useRef, useState, useEffect } from "react"
-import logo from '../../assets/img/webp/logo-zeero-architecture-studio.png'
-
+import logoSrc from '../../assets/img/webp/logo-zeero-architecture-studio.png'
 
 import Navbar from "./navbar"
 
 const Header = () => {
+    // State to track the height of the header
     const [headerHeight, setHeaderHeight] = useState(0)
     const headerRef = useRef()
 
+    // Effect to update header height on mount
     useEffect(() => {
         setHeaderHeight(headerRef.current.offsetHeight)
-    }, []) //Empty dependency array so it only runs once at render
+    }, [])
 
-    return <>
-        {/* Input used as main menu controller for mobile */}
-        <input 
-            hidden
-            type="checkbox" 
-            name="toggle-main-menu-mobile" 
-            id="toggle-main-menu-mobile"
-            className="[&:checked+header_ul]:translate-x-[-100%] md:[&:checked+header_ul]:translate-x-0"
-            // className="[&:checked+header+div]:pointer-events-auto [&:checked+header+div]:bg-[#46464677] [&:checked+header_ul]:translate-x-[-100%] md:[&:checked+header_ul]:translate-x-0"
-        />
-        <header 
-            ref={headerRef} 
-            className="flex justify-center items-center bg-zeero-primary px-zeero py-4 relative z-[999] uppercase tracking-widest"
-        >
-            <div className="flex justify-between items-center flex-1">
-                <Link to='/'>
-                    <h1>
-                        <img 
-                            src={logo}
-                            alt="Zeero logo" 
-                            width={146} 
-                            height={60}
-                            className="w-[146px] h-[60px]"
-                        />
-                    </h1>
-                </Link>
-                <Navbar 
-                    className="relative z-1" 
-                    headerHeight={headerHeight}
-                />
-            </div>
-        </header>
-    </>
+    // Root path for the home link
+    const ROOT_PATH = '/'
+
+    // Define styles for header elements using TailwindCSS classes
+    const styles = {
+        header: "flex justify-center items-center bg-zeero-primary px-zeero py-4 relative z-[999] uppercase tracking-widest",
+        div: "flex justify-between items-center flex-1",
+        img: "w-[146px] h-[60px]",
+        input: "[&:checked+header_ul]:-translate-x-full md:[&:checked+header_ul]:translate-x-0",
+        navbar: "relative z-1"
+    }
+
+    const inputProperties = {
+        hidden : true,
+        type : "checkbox",
+        name : "toggle-main-menu-mobile",
+        id : "toggle-main-menu-mobile",
+        className : styles.input
+    }
+
+    // Logo properties for the image element
+    const logoProperties = {
+        src: logoSrc,
+        alt: 'Logo ZEERO',
+        width: 146,
+        height: 60,
+        className: styles.img
+    }
+
+    return (
+        <>
+            {/* Input used as main menu controller for mobile */}
+            <input {...inputProperties}/>
+            <header
+                ref={headerRef}
+                className={styles.header}
+            >
+                <div className={styles.div}>
+                    {/* Logo with Link to home */}
+                    <Link to={ROOT_PATH}>
+                        <h1>
+                            <img {...logoProperties} />
+                        </h1>
+                    </Link>
+                    <Navbar
+                        className={styles.navbar}
+                        headerHeight={headerHeight}
+                    />
+                </div>
+            </header>
+        </>
+    )
 }
 
 export default Header
--- zeero/src/routes/home.jsx
+++ zeero/src/routes/home.jsx
@@ -1,24 +1,57 @@
+import React from 'react'
 import Header from '../components/header'
 import Slider from '../components/slider'
-import logo from '../assets/img/webp/logo-zeero.png'
+import logoSrc from '../assets/img/webp/logo-zeero.png'
 
 const Home = () => {
-    return <>
-        <Header />
-        <main>
-            <Slider />
-            <section className='h-[75vh] w-full bg-zeero-primary text-zeero-on-primary grid place-items-center px-zeero font-zeero'>
-                <div className='text-center grid gap-8 max-w-zeero mx-auto'>
-                    <h2 className='text-5xl font-light flex items-baseline justify-center gap-4 flex-wrap'>
-                        <img className='block w-[145px] h-[36px]' src={logo} alt="logo zeero" width={150} height={90} /> philosophy
-                    </h2>
-                    <p className='max-w-3xl font-extralight'>
-                        A Zeero Studio ens dediquem a la creació de projectes arquitectònics amb un enfocament cap a la sostenibilitat i l’eficiència energètica, apostant per edificis de consum gairebé zero. El nostre compromís no s’atura en l’estructura externa; ens endinsem en el disseny d’espais interiors, prioritzant la salut i el benestar dels usuaris per a fomentar una millora substancial en la seva qualitat de vida. Cada línia traçada i cada material escollit son decisions preses per generar entre tots un futur més sostenible.
-                    </p>
-                </div>
-            </section>
-        </main>
-    </>
+    // TailwindCSS styles used in this component
+    const styles = {
+        section: 'h-[75vh] w-full bg-zeero-primary text-zeero-on-primary grid place-items-center px-zeero font-zeero',
+        div: 'text-center grid gap-8 max-w-zeero mx-auto',
+        h2: 'text-5xl font-light flex items-baseline justify-center gap-4 flex-wrap',
+        img: 'block w-[145px] h-[36px]',
+        p: 'max-w-3xl font-extralight'
+    }
+
+    // Logo properties
+    const logo = {
+        src: logoSrc,
+        width: 150,
+        height: 90,
+        alt: 'Logo ZEERO'
+    }
+
+    // Generates the title with the logo
+    const generateTitle = () => (
+        <h2 className={styles.h2}>
+            <img
+                {...logo}
+                className={styles.img}
+            /> Philosophy
+        </h2>
+    )
+
+    // Description content
+    const description = (
+        <p className={styles.p}>
+            At Zeero Studio, we focus on creating architectural projects with an emphasis on sustainability and energy efficiency, aiming for nearly zero energy buildings. Our commitment extends beyond the external structure; we delve into the design of interior spaces, prioritizing the health and well-being of users to foster a substantial improvement in their quality of life. Every line drawn and every material chosen are decisions made to collectively generate a more sustainable future.
+        </p>
+    )
+
+    return (
+        <>
+            <Header />
+            <main>
+                <Slider />
+                <section className={styles.section}>
+                    <div className={styles.div}>
+                        {generateTitle()}
+                        {description}
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
 
 export default Home
